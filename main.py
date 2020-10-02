8f35e6f7ad61296ea68ccb881bb1cfb0fe490deb
yk2kus <yogeshkushwaha4@gmail.com>
2020-10-02 10:05:08 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+dict_A = {'key': 'value'}
+
+dict_student = {
+    'S1' : {'height':"I don't want to give"},
+    'S2' : {'age': 16, 'height': 170},
+    'Alexander Miroshnikov': {'age': 11,'weight': '67kg', 'height': 152, 'Gender': 'Male'},
+
+}
+
+
+for key,  value in dict_student.items():
+    print (key, value)
+
+# names_students = ["Binary file Alexander Miroshnikov" ,"Daria Miroshnikova", "Danila Miroshnikov", "Andre"]
+# for stud_name in names_students:
+#     if "a" in stud_name.lower():
+#         print ("name is .......", stud_name)
+
+
+# print (list_student[5])
+# dictionaries
+# controls (loops)
+#
+# ## Gaming
+# pyGame
+
+# list_A = []
+# # print (dir(list_A))
+# print("Address of List in memory before update ", id(list_A))
+# list_A.append(12)
+# print("Address of List in memory after update ", id(list_A))
+# # print (list_A)
+#
+#
+# tuple_A = (1,2)
+# print("Address of Tuple in memory 1st", id(tuple_A))
+# tuple_A = (1,2,3)
+# print("Address of Tuple in memory 2nd", id(tuple_A))
+# # print (dir(tuple_A))
+
+#
